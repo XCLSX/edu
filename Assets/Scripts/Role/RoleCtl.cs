db8f2bb6e30aaee52869b66316056635a2aaae94
hush <hush@hushdeMacBook-Air.local>
2025-02-24 15:20:13 +0800

    [camera] 添加相机跟随

--- Assets/Scripts/Role/RoleCtl.cs
+++ Assets/Scripts/Role/RoleCtl.cs
@@ -29,11 +29,13 @@ namespace Role
         // Update is called once per frame
         void Update()
         {
+            CheckRotation();
+            CheckMove();
         }
 
         void FixedUpdate()
         {
-            CheckMove();
+            // CheckMove();
         }
 
         void CheckMove()
@@ -42,8 +44,34 @@ namespace Role
             var moveVertical = Input.GetAxis("Vertical");
             var moveMovement = new Vector3(moveHorizontal * speed, moveVertical * speed, 0);
             _rigidbody.linearVelocity = moveMovement;
+            
+            // var moveHorizontal = Input.GetAxis("Horizontal");
+            // var moveVertical = Input.GetAxis("Vertical");
+            // var moveMovement = new Vector3(moveHorizontal, moveVertical, 0) * Time.deltaTime;
+            // transform.position += moveMovement * speed;
+            
+            // var moveHorizontal = Input.GetAxis("Horizontal");
+            // var moveVertical = Input.GetAxis("Vertical");
+            // var moveMovement = new Vector3(moveHorizontal, moveVertical, 0) * Time.deltaTime;
+            // transform.position += moveMovement * speed;
         }
 
+        void CheckRotation()
+        {
+            if (Camera.main != null)
+            {
+                Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        
+                // 计算角色与鼠标之间的方向向量
+                Vector2 direction = new Vector2(mousePosition.x - transform.position.x, mousePosition.y - transform.position.y);
+        
+                // 计算角色的角度，使其朝向鼠标
+                float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+        
+                // 旋转角色，设置角色的朝向
+                transform.rotation = Quaternion.Euler(new Vector3(0, 0, angle));
+            }
+        }
         #endregion
 
         #region Public Functions
